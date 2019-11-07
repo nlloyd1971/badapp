c032ae73d8df31ebf985e6e58724c865737899c6
nlloyd1971 <40879623+nlloyd1971@users.noreply.github.com>
2019-11-07 10:23:12 +0000

    Change to 10000000

--- AwesomeWebApp/Controllers/HomeController.cs
+++ AwesomeWebApp/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace AwesomeWebApp.Controllers
    	            bad2 = bad2 * bad2;
     	        bad++;
 
-            } while (bad < 100000000);
+            } while (bad < 10000000);
             ViewData["X"] = DateTime.Now;
             return View();
         }
