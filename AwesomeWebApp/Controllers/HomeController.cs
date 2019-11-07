faf1feea8c9ecd41ff6dca1b7959137a6322aa37
nlloyd1971 <40879623+nlloyd1971@users.noreply.github.com>
2019-11-07 12:10:46 +0000

    10000

--- AwesomeWebApp/Controllers/HomeController.cs
+++ AwesomeWebApp/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace AwesomeWebApp.Controllers
    	            bad2 = bad2 * bad2;
     	        bad++;
 
-            } while (bad < 1000);
+            } while (bad < 10000);
             ViewData["X"] = DateTime.Now;
             return View();
         }
