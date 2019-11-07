64a1090b2f72bfda6758644259c38904e89b89a3
nlloyd1971 <40879623+nlloyd1971@users.noreply.github.com>
2019-11-07 10:04:08 +0000

    Up to 1000

--- AwesomeWebApp/Controllers/HomeController.cs
+++ AwesomeWebApp/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace AwesomeWebApp.Controllers
    	            bad2 = bad2 * bad2;
     	        bad++;
 
-            } while (bad < 100);
+            } while (bad < 1000);
             return View();
         }
 
